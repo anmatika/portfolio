bbe7a2e286af8f9304557dde30f1bda0ab21a382
anmatika <4ntti.matikainen@gmail.com>
2015-04-16 18:03:04 +0300

    blog

--- server/app.js
+++ server/app.js
@@ -1,45 +1,12 @@
-
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
-var exphbs  = require('express-handlebars');
 var router = express.Router();
-var mongoose = require("mongoose");
-var configDb = require('./config/db');
-var passport = require('passport');
-var session      = require('express-session');
-var flash    = require('connect-flash');
-var fs = require('fs');
-var https = require('https');
-var http = require('http');
-var key = fs.readFileSync('key.pem');
-var cert = fs.readFileSync('cert.pem');
 var i18n = require('i18next');
-
-var https_options = {
-    key: key,
-    cert: cert
-};
-
-var PORT = 8000;
-var HOST = 'localhost';
-var api = express();
-
-var hbs = exphbs.create({
-    defaultLayout: 'main',   
-    helpers: require('./lib/helpers')(i18n)
-});
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-require('./config/passport')(passport); // pass passport for configuration
-mongoose.connect(configDb.url);
-
+var flash = require('connect-flash');
+var path = require('path');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -47,18 +14,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// required for passport
-app.use(session({ secret: 'fhappylfjkl3jklovec312yesdvdgood' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-
-// default: using 'accept-language' header to guess language settings
-// app.use(i18n.init);
-// i18n.init({ lng: "en-US" });
 i18n.init({ lng: "fi" });
-
 // i18n.setLng('en-US', function(t) { /* loading done */ });
 
 /**
@@ -111,28 +69,13 @@ app.use(express.static(path.join(__dirname, '/bower_components')));
 // add static files serving for server side scripts
 app.use(express.static(path.join(__dirname, '/scripts')));
 
-//HTTPS Api Server
-https.createServer(https_options, app).listen(8000, function(){
-  console.log("Api server listening on port " + app.get('ssl_port'));
-});
-
-
-function ensureSecure(req, res, next){
-  if(req.secure){
-    // OK, continue
-    return next();
-  };
-  res.redirect('https://'+ req.host +':8000' + req.url); // handle port numbers if you need non defaults
-};
-
-app.all('/secure/*', ensureSecure); // at top of routing calls
 
 // https_server.listen(PORT);
 
 /**
 * Routes
 */
-require('./routes/routes')(router, passport, mongoose);
+require('./routes/main')(router);
 app.use(router);
 
 module.exports = app;
