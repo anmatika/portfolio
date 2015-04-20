406f9f7c3690309f8169d12d82c93f2e05001966
anmatika <4ntti.matikainen@gmail.com>
2015-04-20 18:08:19 +0300

    about me additions

--- client/app/scripts/controllers/about.js
+++ client/app/scripts/controllers/about.js
@@ -8,10 +8,17 @@
  * Controller of the appApp
  */
 angular.module('portfolioApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function ($scope, dataSvc) {
+    $scope.getData = function() {
+			dataSvc.getData('json/professional_history.json')
+				.then(function(data) {
+						console.log('data: ' + data);
+						$scope.histories = data;
+					},
+					function(reason) {
+						console.log(reason);
+					});
+		}
+
+		$scope.getData();
   });
